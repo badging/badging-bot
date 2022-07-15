2158584635373040d0ee2099efd61ce8bb685f16
Kaxada <enocksdha@gmail.com>
2022-07-15 08:23:46 +0300

    refactored index.js to octokit library

--- index.js
+++ index.js
@@ -1,34 +1,43 @@
-const bodyParser = require("body-parser");
+const {App, createNodeMiddleware}  = require('octokit')
 const { parsed: envs } = require("dotenv").config();
-const express = require("express");
 const SmeeClient = require("smee-client");
-const bot = require("./bot");
 
-// create instances
-const app = express();
-var router = express.Router();
-
-// middlewares
-app.use(bodyParser.json());
-app.use(router);
-
-// receive webhook responses from repo
-router.post("/", async (req, res) => {
-  const payload = await req.body;
-  res.sendStatus(200);
-  bot(payload);
+// instantiate Github App
+const app = new App({
+  appId: envs.appId,
+  privateKey: envs.privateKey,
+  oauth: {
+    clientId: envs.clientId,
+    clientSecret: envs.clientSecret,
+  },
+  webhooks: { secret: envs.webhookSecret },
 });
 
-// setup local server
-app.listen(process.env.PORT, () => {
-  console.log(`Server Running on Port ${envs.PORT}`);
-});
+app.webhooks.on("issues.opened", async ({ octokit, payload }) => {
+
+  const { data: { content } } = await octokit.rest.repos.getContent({
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    path: ".github/applicant-welcome.md"
+  })
+
+  await octokit.rest.issues.createComment({
+    owner: payload.repository.owner.login,
+    repo: payload.repository.name,
+    issue_number: payload.issue.number,
+    body: Buffer.from(content, "base64").toString()
+  }).then((res) => console.log(res.status)).catch(err => console.error(err))
+
+})
+
+// create local server to receive webhooks
+require("http").createServer(createNodeMiddleware(app)).listen(envs.PORT);
 
 //connect local server to network client in development
 if (process.env.NODE_ENV !== "production") {
   const smee = new SmeeClient({
     source: "https://smee.io/badging",
-    target: `http://localhost:${process.env.PORT}/`,
+    target: `http://localhost:${process.env.PORT}/api/github/webhooks`,
     logger: console,
   });
   smee.start();
